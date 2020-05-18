f324d815adef18950542eea8d5e6faab5486085d
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2020-05-18 09:51:21 -0700

    Clean-Up on Aisle Gits

---
+++ PFBack/b_project_feed/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Location,Features,Profile,Reviews,Comments,Category
+from .serializers import LocationSerializer,FeaturesSerializer,ProfileSerializer,ReviewsSerializer, CommentsSerializer,CategorySerializer
+
+# Create your views here.
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class FeaturesViewSet(viewsets.ModelViewSet):
+    queryset=Features.objects.all()
+    serializer_class = FeaturesSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset=Profile.objects.all()
+    serializer_class=ProfileSerializer
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset=Reviews.objects.all()
+    serializer_class=ReviewsSerializer
+
+class CommentsViewSet(viewsets.ModelViewSet):
+    queryset=Comments.objects.all()
+    serializer_class=CommentsSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
