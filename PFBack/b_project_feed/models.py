86e2368be6fb11b7ffae6861f40349907464722f
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2020-05-19 10:54:12 -0700

    Magical Anxiety

--- PFBack/b_project_feed/models.py
+++ PFBack/b_project_feed/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+import numpy as np
 import random
 import string
 import re
@@ -25,7 +26,9 @@ def code_registration(length=6):
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE,)
     location = models.CharField(verbose_name="profile", max_length=30,)
-    favorites = models.TextField(verbose_name="user_favorites",default="No Favorites Yet!")
+    favorites = models.ManyToManyField(
+        verbose_name="user_favorites", default="No Favorites Yet!"
+    )
     email_regex = RegexValidator(
         regex=r"^[a-z0-9\._]+@\w+\.\w{2,3}$",
         flags=re.IGNORECASE,
@@ -38,7 +41,7 @@ class Profile(models.Model):
         verbose_name="registration_code", max_length=15, default=code_registration,
     )
     links = models.URLField(verbose_name="shared_links", blank=True,)
-    #image = models.ImageField(blank=True)
+    # image = models.ImageField(blank=True)
 
 
 class Location(models.Model):
@@ -60,14 +63,15 @@ class Location(models.Model):
     zipcode = models.CharField(verbose_name="location_zip", max_length=10,)
     website = models.URLField(verbose_name="location_website", blank=True,)
     phone = models.CharField(verbose_name="location_phone", max_length=10,)
-    business_hours = models.CharField(verbose_name="location_hours",max_length=50)
+    open_close = models.TimeField(verbose_name="location_hours", max_length=50)
     # (Is there a way to make business hours a multiple entry field?)
     res_join = models.DateTimeField(verbose_name="date_res_join", auto_now_add=True,)
     res_mod = models.DateTimeField(verbose_name="date_res_mod", auto_now=True,)
-    #res_image = models.ImageField(verbose_name="restaurant_image", blank=True,)
+    # res_image = models.ImageField(verbose_name="restaurant_image", blank=True,)
 
 
 class Features(models.Model):
+    offered = models.ManyToManyField()
     feat_rating = models.IntegerField(
         verbose_name="feature_rating",
         validators=[MaxValueValidator(5), MinValueValidator(1)],
@@ -79,6 +83,12 @@ class Reviews(models.Model):
         verbose_name="main_rating",
         validators=[MaxValueValidator(5), MinValueValidator(1)],
     )
+    # def average_rating(self):
+    #     all_ratings = map(lambda x: x.rating, self.review_set.all())
+    #     return np.mean(all_ratings)
+
+    # def __unicode__(self):
+    #     return self.name
     user = models.ForeignKey(
         verbose_name="user",
         to=settings.AUTH_USER_MODEL,
@@ -86,18 +96,29 @@ class Reviews(models.Model):
         on_delete=models.CASCADE,
     )
 
+
 class Comments(models.Model):
-    user=models.ForeignKey(User, related_name='user_comments', on_delete=models.SET_NULL, null=True,)
-    review=models.ForeignKey(Reviews, related_name='user_comments', on_delete=models.CASCADE,)
-    content=models.TextField(verbose_name='review_comment',)
-    com_create=models.DateTimeField(verbose_name='date_comment_created', auto_now_add=True)
-    com_mod=models.DateTimeField(verbose_name='date_comment_modified', auto_now=True)
+    user = models.ForeignKey(
+        User, related_name="user_comments", on_delete=models.SET_NULL, null=True,
+    )
+    review = models.ForeignKey(
+        Reviews, related_name="user_comments", on_delete=models.CASCADE,
+    )
+    content = models.TextField(verbose_name="review_comment",)
+    com_create = models.DateTimeField(
+        verbose_name="date_comment_created", auto_now_add=True
+    )
+    com_mod = models.DateTimeField(verbose_name="date_comment_modified", auto_now=True)
+
 
 class Category(models.Model):
-    name=models.CharField(verbose_name="category_name", max_length=10, unique=True,)
+    name = models.CharField(verbose_name="category_name", max_length=10, unique=True,)
+
     def __str__(self):
         return self.name
 
+
 class Favorites(models.Model):
-    user=models.ForeignKey(User, related_name="favorite_places", on_delete=models.SET_NULL, null=True, )
-    
+    user = models.ForeignKey(
+        User, related_name="favorite_places", on_delete=models.SET_NULL, null=True,
+    )
