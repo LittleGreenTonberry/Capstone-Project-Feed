86a1948c784eedc8a698e4b008c183954c68ba8e
CloudF1sh <61166522+CloudF1sh@users.noreply.github.com>
2020-05-27 09:17:29 -0700

    Home Improvement

--- projectfeed_front/projectfeed/src/App.vue
+++ projectfeed_front/projectfeed/src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/rsign">Add a Restaurant</router-link>
+      <router-link to="/signin">Sign-In</router-link>
+      <router-link to="/signup">Sign-Up</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+export default {}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
